55859481214b557cffcc9025983548eb865f0871
HwichulShin <shinhwiiron@gmail.com>
2025-04-01 08:14:30 +0900

    Box Shadow 추가

--- MiniGame/screens/StartGameScreen.js
+++ MiniGame/screens/StartGameScreen.js
@@ -1,14 +1,33 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={style.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+
+    // Shadow Android only
+    elevation: 4,
+
+    // Shadow iOS only
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
 
 export default StartGameScreen;
