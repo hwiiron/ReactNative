c43aa76f754925e0e8af762476cd94865a7ee420
HwichulShin <shinhwiiron@gmail.com>
2025-04-04 09:40:12 +0900

    게임 재시작 추가

--- MiniGame/screens/GameOverScreen.js
+++ MiniGame/screens/GameOverScreen.js
@@ -3,7 +3,7 @@ import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 import PrimaryButton from "../components/ui/PrimaryButton";
 
-function GameOverScreen() {
+function GameOverScreen({ roundNumber, userNumber, onStartNewGame }) {
   return (
     <View style={styles.rootContainer}>
       <Title>Game Over!</Title>
@@ -16,11 +16,12 @@ function GameOverScreen() {
       </View>
 
       <Text style={styles.summaryText}>
-        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
-        guess the number <Text style={styles.highlight}>Y</Text>
+        Your phone needed <Text style={styles.highlight}>{roundNumber}</Text>{" "}
+        rounds to guess the number{" "}
+        <Text style={styles.highlight}>{userNumber}</Text>
       </Text>
 
-      <PrimaryButton>Start New Game</PrimaryButton>
+      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
   );
 }
--- MiniGame/screens/GameScreen.js
+++ MiniGame/screens/GameScreen.js
@@ -30,6 +30,11 @@ function GameScreen({ userNumber, gameOverHandler }) {
     }
   }, [currentGuess, userNumber, gameOverHandler]);
 
+  useEffect(() => {
+    minBoundary = 1;
+    maxBoundary = 100;
+  });
+
   function nextGuessHandler(direction) {
     // direction => 'lower', 'greater'
 
