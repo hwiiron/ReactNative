9080b9c0e86e36ea7ffaf99709c961f764d23a28
HwichulShin <shinhwiiron@gmail.com>
2025-04-04 09:27:16 +0900

    Game Over 수정

--- MiniGame/screens/GameOverScreen.js
+++ MiniGame/screens/GameOverScreen.js
@@ -1,6 +1,7 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function GameOverScreen() {
   return (
@@ -14,7 +15,12 @@ function GameOverScreen() {
         />
       </View>
 
-      <Text>Your phone needed X rounds to guess the number Y</Text>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number <Text style={styles.highlight}>Y</Text>
+      </Text>
+
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -40,5 +46,15 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
